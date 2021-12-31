eedb7cf864c636f4a7e8f0863f2ca4be633fbe36
Giovanni Zenari <zenari12@gmail.com>
2021-12-31 18:33:43 +0100

    Configured Doxygen

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/**
+ *  A test class. A more elaborate class description.
+ */
 int main(int argc, const char** argv) {
 
     printf("Hello World");
---
+++ src/test.h
@@ -0,0 +1,19 @@
+/**
+* @file
+* @author Jan Doe <jandoe@example.com>
+* @version 1.0
+*
+* @section LICENSE
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License as
+* published by the Free Software Foundation; either version 2 of
+* the License, or (at your option) any later version.
+*
+* @section DESCRIPTION
+*
+* The time class represents a moment of time.
+*/
+typedef enum {
+    OP_RETURN,
+} OpCode;
