ebc7b531c6b05ff032ff8a070d6cf5a54fd39075
Giovanni Zenari <zenari12@gmail.com>
2022-01-01 22:00:53 +0100

    New tests on constants

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
 CC=clang
 CFLAGS=-I.
 
-build: ./src/main.o ./src/chunk.o ./src/memory.o ./src/debug.o
-	$(CC) -o ./build/Fuego ./src/main.o ./src/chunk.o ./src/memory.o ./src/debug.o
+build: ./src/main.o ./src/chunk.o ./src/memory.o ./src/debug.o ./src/value.o
+	$(CC) -o ./build/Fuego ./src/main.o ./src/chunk.o ./src/memory.o ./src/debug.o ./src/value.o
 
 run:
 	./build/Fuego
 
-test: ./src/test.o ./src/chunk.o ./src/memory.o ./src/debug.o
-	$(CC) -o ./build/Test ./src/test.o ./src/chunk.o ./src/memory.o ./src/debug.o -lcriterion
+test: ./src/test.o ./src/chunk.o ./src/memory.o ./src/debug.o ./src/value.o
+	$(CC) -o ./build/Test ./src/test.o ./src/chunk.o ./src/memory.o ./src/debug.o ./src/value.o -lcriterion
 
 doc:
 	doxygen
--- src/test.c
+++ src/test.c
@@ -1,4 +1,6 @@
 #include <criterion/criterion.h>
+
+#include "value.h"
 #include "chunk.h"
 
 Test(chunk_suite, basics) {
@@ -8,22 +10,45 @@ Test(chunk_suite, basics) {
     cr_assert_eq(chunk.capacity, 0);
     cr_assert_eq(chunk.count, 0);
     cr_assert_eq(chunk.code, NULL);
+    cr_assert_eq(chunk.constants.values, NULL);
+    cr_assert_eq(chunk.constants.capacity, 0);
+    cr_assert_eq(chunk.constants.count, 0);
 
     freeChunk(&chunk);
 
     cr_assert_eq(chunk.capacity, 0);
     cr_assert_eq(chunk.count, 0);
     cr_assert_eq(chunk.code, NULL);
+    cr_assert_eq(chunk.constants.values, NULL);
+    cr_assert_eq(chunk.constants.capacity, 0);
+    cr_assert_eq(chunk.constants.count, 0);
 
     writeChunk(&chunk, OP_RETURN);
 
     cr_assert_eq(chunk.capacity, 8);
     cr_assert_eq(chunk.count, 1);
     cr_assert_eq(chunk.code[0], OP_RETURN);
+    cr_assert_eq(chunk.constants.values, NULL);
+    cr_assert_eq(chunk.constants.capacity, 0);
+    cr_assert_eq(chunk.constants.count, 0);
+
+    Value value = 2.1;
+    addCostant(&chunk, value);
+
+    cr_assert_eq(chunk.capacity, 8);
+    cr_assert_eq(chunk.count, 1);
+    cr_assert_eq(chunk.code[0], OP_RETURN);
+    cr_assert_eq(chunk.constants.values[0], 2.1);
+    cr_assert_eq(chunk.constants.capacity, 8);
+    cr_assert_eq(chunk.constants.count, 1);
 
     freeChunk(&chunk);
 
     cr_assert_eq(chunk.capacity, 0);
     cr_assert_eq(chunk.count, 0);
     cr_assert_eq(chunk.code, NULL);
+    cr_assert_eq(chunk.constants.values, NULL);
+    cr_assert_eq(chunk.constants.capacity, 0);
+    cr_assert_eq(chunk.constants.count, 0);
 }
+
