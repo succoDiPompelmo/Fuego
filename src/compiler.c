931d4e0da28bbeec24baed32c934c55d84923ed7
Giovanni Zenari <zenari12@gmail.com>
2022-01-19 23:14:31 +0100

    Reading global variables

--- src/compiler.c
+++ src/compiler.c
@@ -188,6 +188,15 @@ static void string() {
                                 parser.previous.length - 2)));
 }
 
+static void namedVariable(Token name) {
+    uint8_t arg = identifierConstant(&name);
+    emitBytes(OP_GET_GLOBAL, arg);
+}
+
+static void variable() {
+    namedVariable(parser.previous);
+}
+
 static void unary() {
     TokenType type = parser.previous.type;
 
@@ -223,7 +232,7 @@ ParseRule rules[] = {
     [TOKEN_EQUAL_EQUAL]     = {NULL,        binary, PREC_EQUALITY},
     [TOKEN_LESS]            = {NULL,        binary, PREC_COMPARISON},
     [TOKEN_LESS_EQUAL]      = {NULL,        binary, PREC_COMPARISON},
-    [TOKEN_IDENTIFIER]      = {NULL,        NULL,   PREC_NONE},
+    [TOKEN_IDENTIFIER]      = {variable,    NULL,   PREC_NONE},
     [TOKEN_STRING]          = {string,      NULL,   PREC_NONE},
     [TOKEN_NUMBER]          = {number,      NULL,   PREC_NONE},
     [TOKEN_AND]             = {NULL,        NULL,   PREC_NONE},
