648f4c2fdf2b742553d2743051cc9d9e22088fe8
Giovanni Zenari <zenari12@gmail.com>
2022-01-24 19:18:41 +0100

    More testing experiment

--- test/test.c
+++ test/test.c
@@ -1,13 +1,10 @@
 #include "unity.h"
 #include "table.h"
+#include "value.h"
+#include "object.h"
 
-void setUp(void)
-{
-}
-
-void tearDown(void)
-{
-}
+void setUp(void) {}
+void tearDown(void) {}
 
 void test_Table(void)
 {
@@ -17,6 +14,15 @@ void test_Table(void)
   TEST_ASSERT_EQUAL(table.capacity, 0);
   TEST_ASSERT_EQUAL(table.count, 0);
   TEST_ASSERT_EQUAL(table.entries, NULL);
+
+  Value boolVal = BOOL_VAL(true);
+  Value numberVal = NUMBER_VAL(1);
+
+  ObjString* key = takeString("key", 3);
+
+  tableSet(&table, key, boolVal);
+
+  TEST_ASSERT_TRUE(tableGet(&table, key, &result));
 }
 
 int main(void) {
