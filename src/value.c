d86eef6b7bc5ed73aec8de39c784d338f4596847
Giovanni Zenari <zenari12@gmail.com>
2022-02-06 17:54:04 +0100

    Nan Boxing

--- src/value.c
+++ src/value.c
@@ -28,23 +28,42 @@ void freeValueArray(ValueArray* array) {
 }
 
 void printValue(Value value) {
-    switch (value.type) {
-        case VAL_BOOL:
+    #ifdef NAN_BOXING
+        if (IS_BOOL(value)) {
             printf(AS_BOOL(value) ? "true": "false");
-            break;
-        case VAL_NIL: printf("nil"); break;
-        case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
-        case VAL_OBJ: printObject(value); break;
-    }
+        } else if (IS_NIL(value)) {
+            printf("nil");
+        } else if (IS_NUMBER(value)) {
+            printf("%g", AS_NUMBER(value));
+        } else if (IS_OBJ(value)) {
+            printObject(value);
+        }
+    #else
+        switch (value.type) {
+            case VAL_BOOL:
+                printf(AS_BOOL(value) ? "true": "false");
+                break;
+            case VAL_NIL: printf("nil"); break;
+            case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
+            case VAL_OBJ: printObject(value); break;
+        }
+    #endif
 }
 
 bool valuesEqual(Value a, Value b) {
-    if (a.type != b.type) return false;
-    switch (a.type) {
-        case VAL_BOOL:      return AS_BOOL(a) == AS_BOOL(b);
-        case VAL_NIL:       return true;
-        case VAL_NUMBER:    return AS_NUMBER(a) == AS_NUMBER(b);
-        case VAL_OBJ:       return AS_OBJ(a) == AS_OBJ(b);
-        default:            return false;
-    }
+    #ifdef NAN_BOXING
+        if (IS_NUMBER(a) && IS_NUMBER(b)) {
+            return AS_NUMBER(a) == AS_NUMBER(b);
+        }
+        return a == b;
+    #else
+        if (a.type != b.type) return false;
+        switch (a.type) {
+            case VAL_BOOL:      return AS_BOOL(a) == AS_BOOL(b);
+            case VAL_NIL:       return true;
+            case VAL_NUMBER:    return AS_NUMBER(a) == AS_NUMBER(b);
+            case VAL_OBJ:       return AS_OBJ(a) == AS_OBJ(b);
+            default:            return false;
+        }
+    #endif
 }
